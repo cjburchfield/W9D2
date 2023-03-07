f998522bc9d99d567e55230eabbd89837582533c
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-03-07 13:57:56 -0500

    Adding skeleton

---
+++ skeleton/src/index.js
@@ -0,0 +1,8 @@
+// const View = // require appropriate file
+// const Game = // require appropriate file
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   // Your code here
+// });
+
+console.log("I'm alive!")
