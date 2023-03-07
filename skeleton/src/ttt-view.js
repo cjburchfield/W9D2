a0fe3b6a3ce630ebc4763866f113f58fcc288ba9
Gary <geehor@gmail.com>
2023-03-07 15:37:53 -0500

    Manage to render the grid view

--- skeleton/src/ttt-view.js
+++ skeleton/src/ttt-view.js
@@ -1,16 +1,22 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard(el);
+  }
 
   setupBoard() {
     const grid = document.createElement("ul");
+
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         const cell = document.createElement("li");
-        cell.dataset.pos = `[${i}, ${j}]`; 
-        grid.appendChild(cell) 
+        cell.dataset.pos = JSON.stringify([i, j]); 
+        grid.append(cell);
       }
     }
 
+    this.el.append(grid);
   }
   
   bindEvents() {}
