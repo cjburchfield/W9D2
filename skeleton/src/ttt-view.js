425ac9d29ffc1211edb7e280f6cf977287aecb87
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-03-07 14:49:28 -0500

    Set up board

--- skeleton/src/ttt-view.js
+++ skeleton/src/ttt-view.js
@@ -1,7 +1,17 @@
 class View {
   constructor(game, el) {}
 
-  setupBoard() {}
+  setupBoard() {
+    const grid = document.createElement("ul");
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const cell = document.createElement("li");
+        cell.dataset.pos = `[${i}, ${j}]`; 
+        grid.appendChild(cell) 
+      }
+    }
+
+  }
   
   bindEvents() {}
 
